4fbad4060963fc3c4fb3a7244874c099bce37529
khushboogupta2001 <138224225+khushboogupta2001@users.noreply.github.com>
2023-07-24 23:19:09 +0530

    Create PDFSplitter.java

---
+++ src/PDFSplitter.java
@@ -0,0 +1,15 @@
+
+package pdfsplitter;
+
+
+public class PDFSplitter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+Dashboard db = new Dashboard();  
+db.setVisible(true);
+    }
+    
+}
